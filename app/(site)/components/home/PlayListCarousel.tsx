ef636f837662a92716a7f2d0f7014d65ad2c5447
jaehan <kyobo.jaehan@gmail.com>
2024-07-31 16:11:58 +0900

    Home > PlayList 개발

---
+++ app/(site)/components/home/PlayListCarousel.tsx
@@ -0,0 +1,64 @@
+import {ReactNode} from 'react';
+import {Playlist} from '@/model/playlist';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import PlayListCard from '@/app/(site)/components/home/PlayListCard';
+
+
+
+type PhotoListCarouselProps = {
+  title: string;
+  subTitle?: string;
+  thumbnail?: ReactNode;
+  playlistArray?: Playlist[]
+
+}
+export default function PlayListCarousel({title, subTitle, thumbnail, playlistArray}: PhotoListCarouselProps) {
+  return(
+    <div className='w-full'>
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+      >
+        <div className='flex flex-row justify-between items-end'>
+          <article className='flex flex-row gap-3 justify-between items-center'>
+            {thumbnail}
+            <div className='flex flex-col justify-center'>
+              <div>
+                {subTitle && <div className='text-neutral-500'>{subTitle}</div>}
+              </div>
+              <div className='text-[34px] font-bold leading-[34px]'>{title}</div>
+            </div>
+          </article>
+
+          <div className='relative'>
+            <div className='absolute left-[-45px] bottom-[20px]'>
+              <CarouselPrevious className='right-2'/>
+              <CarouselNext className='left-2'/>
+            </div>
+
+          </div>
+        </div>
+
+        <CarouselContent className='mt-5'>
+          {
+            playlistArray?.map((playlist, index)=>{
+              return(
+                <CarouselItem key={index} className=" sm:basis-1/2 md:basis-1/4 lg:basis-1/5" >
+                  <PlayListCard playlist={playlist}/>
+                </CarouselItem>
+              )
+            })
+          }
+        </CarouselContent>
+
+      </Carousel>
+    </div>
+  )
+}
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,27 @@
-import Category from '@/app/(site)/components/Category';
+import Category from '@/app/(site)/components/home/Category';
+import PagePadding from '@/components/PagePadding';
+import PlayListCarousel from '@/app/(site)/components/home/PlayListCarousel';
+import {dummyPlaylistArray} from '@/lib/dummyData';
+import UserIcon from '@/components/UserIcon';
 
 export default async function Page  () {
+  const dummyPlaylistArray1 = [...dummyPlaylistArray]
   return (
-    <div className="min-h-[600px]">
-      <div className='mt-9'>
-        <Category/>
+    <PagePadding>
+      <div className="min-h-[600px]">
+        {/* 카테고리 */}
+        <div className='mt-9'>
+          <Category/>
+        </div>
+
+        {/* 플레이 리스트 */}
+        <div className='mt-12'>
+          <PlayListCarousel playlistArray={[...dummyPlaylistArray1]} thumbnail={<div className='w-[56px] h-[56px]' > <UserIcon size={'lg'} /> </div>} title='다시듣기' subTitle='도도' />
+        </div>
+
       </div>
-    </div>
+    </PagePadding>
+
+
   );
 }
